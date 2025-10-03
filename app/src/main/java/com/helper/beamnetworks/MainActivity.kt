14346007aac39f2ab2c9b9a66ec6c42d39a2ac44
ken-neddy <neddykenzz@gmail.com>
2025-10-03 20:46:51 +0300

    Beam networks ops app

--- app/src/main/java/com/helper/beamnetworks/MainActivity.kt
+++ app/src/main/java/com/helper/beamnetworks/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.helper.beamnetworks.ui.theme.BeamNetworksTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +25,25 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BeamNetworksTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun ButtonCard() {
+    Box(modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.TopStart
+    ){
+        Card(modifier = Modifier.padding(16.dp).width(150.dp).height(150.dp),
 
+        ) { }
+    }
+}
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     BeamNetworksTheme {
-        Greeting("Android")
+        ButtonCard()
     }
 }
