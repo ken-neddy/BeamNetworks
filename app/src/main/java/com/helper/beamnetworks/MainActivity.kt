43daae3e262107bb9d6a4b53a962cda7a07cba03
KEN <neddykenzz@gmail.com>
2025-11-08 11:34:21 +0300

    Added Payhero's lipwa link and klizyentp system access inside the app for efficient ops.

--- app/src/main/java/com/helper/beamnetworks/MainActivity.kt
+++ app/src/main/java/com/helper/beamnetworks/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.helper.beamnetworks
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -28,6 +31,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -82,11 +86,19 @@ fun MainScreen(navController: NavController) {
         }
     ) { innerPadding ->
         Row(
-            modifier = Modifier.padding(innerPadding).fillMaxWidth(),
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxWidth(),
             horizontalArrangement = Arrangement.Center
         ) {
-            LogInstallationCard(navController = navController)
-            LogExpenseCard()
+            Column {
+                LogInstallationCard(navController = navController)
+                LipwaLink()
+            }
+            Column {
+                LogExpenseCard()
+                PppoeListCard()
+            }
         }
     }
 }
@@ -96,7 +108,10 @@ fun MainScreen(navController: NavController) {
 fun LogInstallationCard(navController: NavController) {
     Card(
         onClick = { navController.navigate("log_installation") },
-        modifier = Modifier.padding(16.dp).width(150.dp).height(150.dp),
+        modifier = Modifier
+            .padding(16.dp)
+            .width(150.dp)
+            .height(150.dp),
         colors = CardDefaults.cardColors(
             containerColor = Color.White
         ),
@@ -104,11 +119,16 @@ fun LogInstallationCard(navController: NavController) {
             defaultElevation = 7.dp
         )
     ) {
-        Box(modifier = Modifier.fillMaxSize().padding(16.dp)) {
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)) {
             Icon(
                 painter = painterResource(id = R.drawable.installation_hammer_screwdriver),
                 contentDescription = "Log an installation",
-                modifier = Modifier.size(30.dp).align(Alignment.Center).offset(y = (-10).dp),
+                modifier = Modifier
+                    .size(30.dp)
+                    .align(Alignment.Center)
+                    .offset(y = (-10).dp),
                 tint = Color.Unspecified
             )
             Text(
@@ -124,7 +144,10 @@ fun LogInstallationCard(navController: NavController) {
 fun LogExpenseCard() {
     Card(
         onClick = {},
-        modifier = Modifier.padding(16.dp).width(150.dp).height(150.dp),
+        modifier = Modifier
+            .padding(16.dp)
+            .width(150.dp)
+            .height(150.dp),
         colors = CardDefaults.cardColors(
             containerColor = Color.White
         ),
@@ -132,15 +155,102 @@ fun LogExpenseCard() {
             defaultElevation = 10.dp
         )
     ) {
-        Box(modifier = Modifier.fillMaxSize().padding(16.dp)) {
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)) {
             Icon(
                 painter = painterResource(id = R.drawable.currency_usd),
                 contentDescription = "Log an Expense",
-                modifier = Modifier.size(35.dp).align(Alignment.Center).offset(y = (-10).dp),
+                modifier = Modifier
+                    .size(35.dp)
+                    .align(Alignment.Center)
+                    .offset(y = (-10).dp),
                 tint = Color.Unspecified
             )
             Text(
                 text = "Log an Expense",
+                modifier = Modifier.align(Alignment.BottomCenter)
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LipwaLink() {
+    val context = LocalContext.current
+    val url = "https://app.payhero.co.ke/lipwa/1787"
+    Card(
+        onClick = { 
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            context.startActivity(intent)
+        },
+        modifier = Modifier
+            .padding(16.dp)
+            .width(150.dp)
+            .height(150.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White
+        ),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 10.dp
+        )
+    ) {
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)) {
+            Icon(
+                painter = painterResource(id = R.drawable.currency_usd),
+                contentDescription = "Lipwa Link",
+                modifier = Modifier
+                    .size(35.dp)
+                    .align(Alignment.Center)
+                    .offset(y = (-10).dp),
+                tint = Color.Unspecified
+            )
+            Text(
+                text = "Lipwa Link",
+                modifier = Modifier.align(Alignment.BottomCenter)
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PppoeListCard() {
+    val context = LocalContext.current
+    val url = "http://router.klizyentp.com/pppoe/list"
+    Card(
+        onClick = { 
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            context.startActivity(intent)
+        },
+        modifier = Modifier
+            .padding(16.dp)
+            .width(150.dp)
+            .height(150.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White
+        ),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 10.dp
+        )
+    ) {
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)) {
+            Icon(
+                painter = painterResource(id = R.drawable.currency_usd),
+                contentDescription = "PPPoE List",
+                modifier = Modifier
+                    .size(35.dp)
+                    .align(Alignment.Center)
+                    .offset(y = (-10).dp),
+                tint = Color.Unspecified
+            )
+            Text(
+                text = "PPPoE List",
                 modifier = Modifier.align(Alignment.BottomCenter)
             )
         }
